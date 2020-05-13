c567d2691d7dd14b19a04e85fb5d7f133c11563a
cahityusuf <56351404+cahityusuf@users.noreply.github.com>
2020-05-13 10:39:01 +0300

    authorization islemi tamamlandi

--- auth/src/controllers/currentUserController.ts
+++ auth/src/controllers/currentUserController.ts
@@ -1,17 +1,7 @@
 import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 
-const currentUserController = async (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (error) {
-    res.send({ currentUser: null });
-  }
+const currentUserController = (req: Request, res: Response) => {
+  res.send({ currentUser: req.currentUser || null });
 };
 
 export { currentUserController };
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 import { currentUserController } from '../controllers/currentUserController';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUserController);
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  requireAuth,
+  currentUserController
+);
 
 export { router as currentUserRouter };
