6576ce76d06598f9e17dac57bf2f2c5715123b94
cahityusuf <56351404+cahityusuf@users.noreply.github.com>
2020-05-14 07:11:32 +0300

    test islemi tamamlandi

---
+++ auth/src/routes/__test__/signout-test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('clears the cookie after signing out', async () => {
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(200);
+
+  console.log(response.get('Set-Cookie'));
+  // expect(response.get('Set-Cookie')[0]).toEqual();
+});
