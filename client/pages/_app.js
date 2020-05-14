c7bd99f7521a3b507fe66e1de5c489b34e44dc00
cahityusuf <56351404+cahityusuf@users.noreply.github.com>
2020-05-14 21:21:08 +0300

    signin,signup tamamlandı

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
   return (
   <div>
     <h1>Header</h1>
@@ -9,3 +10,26 @@ export default ({ Component, pageProps }) => {
   );
   
 };
+
+AppComponent.getInitialProps = async appContext => {
+
+  console.log(appContext);
+  
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  console.log(data);
+  return data;
+  
+  // let pageProps ={};
+  // if (appContext.Component.getInitialProps) {
+  //    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  // }
+
+  // return {
+  //   pageProps,
+  //   ...data
+  // };
+};
+
+export default AppComponent;
